ee48ba157700891deb103fbca23d60ba0491e8bc
Haislich <josedlv936@gmail.com>
2024-11-25 22:17:26 +0100

    updated

--- vae.py
+++ vae.py
@@ -6,14 +6,14 @@ import numpy as np
 
 class __Encoder(nn.Module):
     def __init__(self, latent_space_dimension, *, stride=2):
-        self.latent_space_dimension = latent_space_dimension
+        super().__init__()
         self.relu_conv1 = nn.Conv2d(3, 32, 4, stride=stride)
         self.relu_conv2 = nn.Conv2d(32, 64, 4, stride=stride)
         self.relu_conv3 = nn.Conv2d(64, 128, 4, stride=stride)
         self.relu_conv4 = nn.Conv2d(128, 256, 4, stride=stride)
 
-        self.dense_mu = nn.Linear(2 * 2 * 256, latent_space_dimension)
-        self.dense_sigma = nn.Linear(2 * 2 * 256, latent_space_dimension)
+        self.fc_mu = nn.Linear(2 * 2 * 256, latent_space_dimension)
+        self.fc_sigma = nn.Linear(2 * 2 * 256, latent_space_dimension)
 
     def forward(self, x):
         x = F.relu(self.relu_conv1(x))
@@ -22,43 +22,44 @@ class __Encoder(nn.Module):
         x = F.relu(self.relu_conv4(x))
         # Flatten the tensor for dense layers
         x = x.view(x.size(0), -1)
-        mu = self.dense_mu(x)
-        sigma = self.dense_sigma(x)
-        epsilon = torch.randn_like(sigma)
-        z = mu + sigma * epsilon
-        return z, mu, sigma
+        mu = self.fc_mu(x)
+        log_sigma = self.fc_sigma(x)
+
+        # This reparametrization trick allow us to still be able to train it
+        return mu, log_sigma
 
 
-class __Decoder(nn.Module): ...
+class __Decoder(nn.Module):
+    def __init__(self, latent_space_dimension, image_chanels, *, stride=2):
+        super().__init__()
+        self.fc= nn.Linear(latent_space_dimension, 1024)
+        self.relu_deconv1 = nn.ConvTranspose2d(1024, 128, 5, stride=stride)
+        self.relu_deconv2 = nn.ConvTranspose2d(128, 64, 5, stride=stride)
+        self.relu_deconv3 = nn.ConvTranspose2d(64, 32, 6, stride=stride)
+        self.sigmoid_deconv = nn.ConvTranspose2d(32, image_chanels, 6, stride=stride)
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.unsqueeze(-1).unsqueeze(-1)
+
+        x = F.relu(self.relu_deconv1(x))
+        x = F.relu(self.relu_deconv2(x))
+        x = F.relu(self.relu_deconv3(x))
+        # https://github.com/pytorch/pytorch/issues/65910
+        return torch.sigmoid(self.sigmoid_deconv(x))
 
 
 class ConvVAE(nn.Module):
-    def __init__(
-        self,
-        latent_space_dimension=32,
-    ):
+    def __init__(self, latent_space_dimension=32, image_channels=3):
         super().__init__()
         self.latent_space_dimension = latent_space_dimension
         # https://worldmodels.github.io/#:~:text=each%20convolution%20and%20deconvolution%20layer%20uses%20a%20stride%20of%202.
         self.encoder = __Encoder(latent_space_dimension, stride=2)
+        self.decoder = __Decoder(latent_space_dimension, image_channels, stride=2)
 
-
-# if __name__ == "__main__":
-#     import gymnasium as gym
-
-#     env = gym.make("CarRacing-v2", continuous=False)
-#     n_episodes = 10000
-#     rewards = []
-#     for episode in range(n_episodes):
-#         total_reward = 0
-#         done = False
-#         s, _ = env.reset()
-#         while not done:
-#             action = env.action_space.sample()
-#             s, reward, terminated, truncated, info = env.step(action)
-#             print(s.shape)
-#             done = terminated or truncated
-#             total_reward += reward
-
-#         rewards.append(total_reward)
-#     print("Hello")
+    def forward(self, x):
+        mu, log_sigma = self.encoder(x)
+        sigma = log_sigma.exp()
+        z = mu + sigma * torch.randn_like(sigma)
+        reconstruction = self.decoder(z)
+        return reconstruction
